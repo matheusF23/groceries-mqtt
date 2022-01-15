fd2a06f2332dbf610517cb6b33d4071a0fbd60a1
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2022-01-14 21:16:09 -0300

    feat: last changes

--- client/client.js
+++ client/client.js
@@ -1,9 +1,11 @@
 const mqtt = require('mqtt')
 
-const client = mqtt.connect('mqtt://test.mosquitto.org')
+const app = require('./app')
 
 const response_topic = 'groceries-response'
 const request_topic = 'groceries-request'
+
+const client = mqtt.connect('mqtt://test.mosquitto.org')
 
 client.on('connect', () => {
   console.log("Conectado ao broker MQTT")
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const mqtt = require('mqtt')
 
-const server = mqtt.connect('mqtt://test.mosquitto.org')
+const app = require('./app')
 
 const response_topic = 'groceries-response'
 const request_topic = 'groceries-request'
+
+const server = mqtt.connect('mqtt://test.mosquitto.org')
 
 server.on('connect', () => {
   console.log("Conectado ao broker MQTT")
@@ -20,6 +22,8 @@ server.on("message", (topic, message) => {
   if (topic === request_topic) {
     try {
       const msg = message.toString().trim()
+      console.log(`[x] Mensagem recebida: ${msg}`)
+
       const response = app(msg)
 
       server.publish(response_topic, response, { qos: 2 })
